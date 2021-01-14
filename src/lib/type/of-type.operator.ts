5805205871ccda18ea39edf07c7b97cdcfd7c37f
achugaev <achugaev93@gmail.com>
2021-01-15 00:27:03 +0200

    feat(type): add ofType operator

---
+++ src/lib/type/of-type.operator.ts
@@ -0,0 +1,19 @@
+import { Observable, OperatorFunction } from 'rxjs';
+import { filter } from 'rxjs/operators';
+import { Type } from '@angular/core';
+
+/**
+ * Selects values matching type(s).
+ * @param types Allowed types
+ * @since 11.3.0
+ * @author Alex Chugaev
+ */
+export function ofType<I extends object, O extends I>(...types: Array<Type<O>>): OperatorFunction<I, O> {
+  return (source: Observable<I>): Observable<O> => {
+    const isInstanceOf = (object: object): object is O => {
+      return !!types.find(type => object instanceof type);
+    };
+
+    return source.pipe(filter(isInstanceOf));
+  };
+}
--- src/public-api.ts
+++ src/public-api.ts
@@ -4,3 +4,5 @@
 
 export * from './lib/task/task-state';
 export * from './lib/task/to-task.operator';
+
+export * from './lib/type/of-type.operator';
